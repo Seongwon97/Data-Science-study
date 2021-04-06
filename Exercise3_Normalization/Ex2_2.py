4c10540a8e1409557ed74a59631fd1f5c85461e7
Seongwon97 <osw970325@naver.com>
2021-04-06 13:17:16 +0900

    Ex2_2

---
+++ Exercise3_Normalization/Ex2_2.py
@@ -0,0 +1,26 @@
+import numpy as np
+score = [28, 35, 26, 32, 28, 28, 35, 34, 46, 42, 37]
+mean =  sum(score) / len(score)
+print("The mean: ",round(mean,2))
+
+sd = np.std(score)
+print("The standard deviation: ",round(sd,2))
+sc=[]
+for i in score:
+    z= (i-mean)/sd
+    sc.append(z)
+
+print("The standard scores: ")
+for i in sc:
+    print(round(i,2), end=" ")
+
+print("\nThe student scores",end=" ")
+count=0
+for i in range(0,len(sc)):
+    if sc[i]<-1 and count==0:
+        print(score[i], end=" ")
+        count+=1
+    elif sc[i]<-1 and count==1:
+        print("and",score[i], end=" ")
+
+print("stand out to receive an F")
