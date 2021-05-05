7a21fe3161c04a422ebf55c369dc35d01c64251a
Seongwon97 <osw970325@naver.com>
2021-05-05 22:13:30 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import sklearn.linear_model as lm
+
+
+df =pd.DataFrame({
+    'x1':np.random.randint (-100, 100, 1000). astype(float),
+    'y1': np.random.randint (-80, 80, 1000). astype(float),
+    'z1': np.random.randint (-150, 150, 1000). astype(float),
+})
+xxx= preprocessing.Normalizer
+plt.show()
